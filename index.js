2263ec058f6321345cd8a6c75cf227c785813fad
Collins Kathurima <collinskathu313@gmail.com>
2024-10-17 23:46:37 +0300

    completed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById('ageForm').addEventListener('submit', function(event) {
 
     const dobInput = document.getElementById('dob').value;
     const currentDateInput = document.getElementById('currentDate').value;
-
+const name = document.getElementById('name').value;
     if (!dobInput || !currentDateInput) {
         alert('kindly fill both Date of Birth and Current Date.');
         return;
@@ -31,4 +31,36 @@ document.getElementById('ageForm').addEventListener('submit', function(event) {
     document.getElementById('Months').textContent = months;
     document.getElementById('Days').textContent = days;
 });
+const form = document.getElementById('user-form');
+const resultsDiv = document.getElementById('output')
+
+// Handle form submission
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const birthdate = document.getElementById('birthdate').value;
+
+    // Send POST request to save user
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            
+                id: 1,
+                name: "Collins",
+                dob: "2005-08-10",
+                currentDate: "2024-10-17",
+                calculatedAge: {
+                  years: 19,
+                  months: 2,
+                  days: 7
+                }
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    })
 
